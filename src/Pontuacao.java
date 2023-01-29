8de4c94c1477fb77ed521a7d5cb7cd51c28f2245
André Alves Correia <andre2013aguabranca@gmail.com>
2023-01-29 10:24:20 -0300

    refatoracao

--- src/Pontuacao.java
+++ src/Pontuacao.java
@@ -7,34 +7,34 @@ public class Pontuacao{
 		private long pontos;
 		private String tipo;
 		
-		Pontuacao(String usuario, long pontos, String tipo){
-			if(pontos < 1) 
-				throw new PontuacaoInvalidaException("Pontos não podem ser inferiores a 1");
-			if(usuario.equals("")) 
-				throw new PontuacaoInvalidaException("Usuário inválido (\"\") ");
-			if(tipo.equals("")) 
-				throw new PontuacaoInvalidaException("Tipo inválido (\"\") ");
-			
+		Pontuacao(String usuario, long pontos, String tipo) 
+				throws PontuacaoInvalidaException {
+			validarPontuacao(usuario, pontos, tipo);
 			this.usuario = usuario;
 			this.pontos = pontos;
 			this.tipo = tipo;
 		}
-		Pontuacao(JSONObject json){
+		Pontuacao(JSONObject json) 
+				throws PontuacaoInvalidaException {
 			long pontos = (long) json.get("pontos");
 			String usuario = (String) json.get("usuario");
 			String tipo = (String) json.get("tipo");
-			
+			validarPontuacao(usuario, pontos, tipo);
+			this.usuario = usuario;
+			this.pontos = pontos;
+			this.tipo = tipo;
+		}
+		
+		private void validarPontuacao(String usuario, long pontos, String tipo) 
+				throws PontuacaoInvalidaException {
 			if(pontos < 1) 
 				throw new PontuacaoInvalidaException("Pontos não podem ser inferiores a 1");
 			if(usuario.equals("")) 
 				throw new PontuacaoInvalidaException("Usuário inválido (\"\") ");
 			if(tipo.equals("")) 
 				throw new PontuacaoInvalidaException("Tipo inválido (\"\") ");
-			
-			this.usuario = usuario;
-			this.pontos = pontos;
-			this.tipo = tipo;
 		}
+		
 		@SuppressWarnings("unchecked")
 		public JSONObject toJSONObject() {
 			JSONObject jsonObj = new JSONObject();
