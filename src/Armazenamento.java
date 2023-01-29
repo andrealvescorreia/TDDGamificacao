ff036c8659a0bc7ec859ddae0cecfa7ccd9fb2a8
André Alves Correia <andre2013aguabranca@gmail.com>
2023-01-29 11:29:38 -0300

    throws

--- src/Armazenamento.java
+++ src/Armazenamento.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 
+import excecoes.PontuacaoInvalidaException;
+
 public interface Armazenamento {
-	public void guardarPontuacao(String usuario, long pontos, String tipo);
+	public void guardarPontuacao(String usuario, long pontos, String tipo) 
+			throws PontuacaoInvalidaException;
 	public long recuperarPontos(String usuario, String tipo);
 	public ArrayList<String> recuperarUsuariosRegistrados();
 	public ArrayList<String> recuperarTiposPontuacao(String usuario);
