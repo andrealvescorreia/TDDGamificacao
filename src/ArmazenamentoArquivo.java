c8ff743e79e32939195dac742b7aa2f94a21b05d
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2023-02-08 10:37:11 -0300

    refatoracao

--- src/ArmazenamentoArquivo.java
+++ src/ArmazenamentoArquivo.java
@@ -26,7 +26,7 @@ public class ArmazenamentoArquivo implements Armazenamento {
 		}
 	}
 	
-	private void recuperarDadosSalvosEmArquivo() throws IOException{
+	private void recuperarDadosSalvosEmArquivo() throws IOException {
 		JSONParser parser = new JSONParser();
 		JSONArray jsonPontuacoes;
 		try {
@@ -50,8 +50,8 @@ public class ArmazenamentoArquivo implements Armazenamento {
 	@Override
 	public void guardar(Pontuacao novaPontuacao) 
 			throws FalhaNoArmazenamentoException {
-		for(Pontuacao pontuacaoExistente : _cachePontuacoes) {
-			if(pontuacaoExistente.getUsuario().equals(novaPontuacao.getUsuario()) && pontuacaoExistente.getTipo().equals(novaPontuacao.getTipo())) {
+		for (Pontuacao pontuacaoExistente : _cachePontuacoes) {
+			if (mesmoUsuarioETipo(pontuacaoExistente, novaPontuacao)) {
 				pontuacaoExistente.addPontos(novaPontuacao.getPontos());
 				salvarCacheNoArquivo();
 				return;
@@ -60,6 +60,12 @@ public class ArmazenamentoArquivo implements Armazenamento {
 		_cachePontuacoes.add(novaPontuacao);
 		salvarCacheNoArquivo();
 	}
+	
+	private boolean mesmoUsuarioETipo(Pontuacao p1, Pontuacao p2) {
+		return (p1.getUsuario().equals(p2.getUsuario()) 
+			 && p1.getTipo().equals(p2.getTipo()));
+	}
+	
 	
 	private void salvarCacheNoArquivo(){
 		try {
@@ -73,16 +79,16 @@ public class ArmazenamentoArquivo implements Armazenamento {
 	
 	@SuppressWarnings("unchecked")
 	private String jsonDoCacheDePontuacoes() {
-		JSONArray jsonArrayPontuacoes = new JSONArray();
+		JSONArray jsonPontuacoes = new JSONArray();
 		for(Pontuacao pontuacao : _cachePontuacoes) 
-			jsonArrayPontuacoes.add(pontuacao.toJSONObject());
-		return jsonArrayPontuacoes.toJSONString();
+			jsonPontuacoes.add(pontuacao.toJSONObject());
+		return jsonPontuacoes.toJSONString();
 	}
 	
 	@Override
 	public long recuperarPontos(String usuario, String tipo) {
-		for(Pontuacao pontuacao : _cachePontuacoes) {
-			if(usuario.equals(pontuacao.getUsuario()) && tipo.equals(pontuacao.getTipo()))
+		for (Pontuacao pontuacao : _cachePontuacoes) {
+			if (usuario.equals(pontuacao.getUsuario()) && tipo.equals(pontuacao.getTipo()))
 				return pontuacao.getPontos();
 		}
 		return 0;
@@ -91,9 +97,9 @@ public class ArmazenamentoArquivo implements Armazenamento {
 	@Override
 	public ArrayList<String> recuperarUsuariosRegistrados() {
 		var usuarios = new ArrayList<String>();
-		for(Pontuacao pontuacao : _cachePontuacoes) {
+		for (Pontuacao pontuacao : _cachePontuacoes) {
 			String usuario = pontuacao.getUsuario();
-			if(usuarios.contains(usuario)) continue;
+			if (usuarios.contains(usuario)) continue;
 			usuarios.add(usuario);
 		}
 		return usuarios;
@@ -102,10 +108,10 @@ public class ArmazenamentoArquivo implements Armazenamento {
 	@Override
 	public ArrayList<String> recuperarTiposDePonto(String usuario) {
 		var tiposDePontosDoUsuario = new ArrayList<String>();
-		for(Pontuacao pontuacao : _cachePontuacoes) {
-			if(tiposDePontosDoUsuario.contains(pontuacao.getTipo())) 
+		for (Pontuacao pontuacao : _cachePontuacoes) {
+			if (tiposDePontosDoUsuario.contains(pontuacao.getTipo())) 
 				continue;
-			if(usuario.equals(pontuacao.getUsuario()))
+			if (usuario.equals(pontuacao.getUsuario()))
 				tiposDePontosDoUsuario.add(pontuacao.getTipo());
 		}
 		return tiposDePontosDoUsuario;
--- src/Pontuacao.java
+++ src/Pontuacao.java
@@ -6,89 +6,108 @@ import pontuacao.excecoes.ObjetoJsonIvalidoException;
 import pontuacao.excecoes.PontuacaoInvalidaException;
 
 public class Pontuacao{
-		private String usuario;
-		private long pontos;
-		private String tipo;
-		
-		Pontuacao(String usuario, long pontos, String tipo) 
-				throws PontuacaoInvalidaException {
-			validarPontuacao(usuario, pontos, tipo);
-			this.usuario = usuario;
-			this.pontos = pontos;
-			this.tipo = tipo;
-		}
-		Pontuacao(JSONObject jsonPontuacao) 
-				throws PontuacaoInvalidaException {
-			validarJSONObject(jsonPontuacao);
-			long pontos = (long)jsonPontuacao.get("pontos");
-			String usuario = (String) jsonPontuacao.get("usuario");
-			String tipo = (String) jsonPontuacao.get("tipo");
-			validarPontuacao(usuario, pontos, tipo);
-			this.usuario = usuario;
-			this.pontos = pontos;
-			this.tipo = tipo;
-		}
-		private void validarPontuacao(String usuario, long pontos, String tipo) 
-				throws PontuacaoInvalidaException {
-			if(pontos < 1) 
-				throw new PontuacaoInvalidaException("Pontos não podem ser inferiores a 1");
-			if(usuario.equals("")) 
-				throw new PontuacaoInvalidaException("Usuário inválido (\"\")");
-			if(usuario.contains(" "))
-				throw new PontuacaoInvalidaException("Usuário inválido (possui espaço(s))");
-			if(tipo.equals("")) 
-				throw new PontuacaoInvalidaException("Tipo inválido (\"\")");
-			if(tipo.contains(" "))
-				throw new PontuacaoInvalidaException("Tipo inválido (possui espaço(s))");
-		}
-		
-		private void validarJSONObject(JSONObject jsonPontuacao) {
-			if(objetoJsonFaltaChaveValida(jsonPontuacao)) 
-				throw new ObjetoJsonIvalidoException("Objeto Json em formato incorreto (faltando chave(s) valida(s))");
-			if(objetoJsonFaltaValorDeTipoValido(jsonPontuacao))
-				throw new ObjetoJsonIvalidoException("Objeto Json em formato incorreto (valor(es) de tipo(s) invalido(s))");
-			
-		}
-		private boolean objetoJsonFaltaChaveValida(JSONObject jsonPontuacao) {
-			return (jsonPontuacao.get("pontos")  == null 
-				 || jsonPontuacao.get("usuario") == null 
-				 || jsonPontuacao.get("tipo")    == null);
-		}
-		
-		private boolean objetoJsonFaltaValorDeTipoValido(JSONObject jsonPontuacao) {
-			return !(jsonPontuacao.get("usuario") instanceof String 
-				  && jsonPontuacao.get("pontos")  instanceof Long
-				  && jsonPontuacao.get("tipo")    instanceof String);
-		}
-		
-		public void addPontos(long pontos) {
-			if(pontos < 1) 
-				throw new AdicionarPontosInvalidosException("Pontos não podem ser inferiores a 1");
-			this.pontos += pontos;
-		}
-		
-		
-		public String toJSONString() {
-			return this.toJSONObject().toJSONString();
-		}
-		
-		@SuppressWarnings("unchecked")
-		public JSONObject toJSONObject() {
-			JSONObject jsonObj = new JSONObject();
-			jsonObj.put("usuario", usuario);
-			jsonObj.put("pontos", pontos);
-			jsonObj.put("tipo", tipo);
-			return jsonObj;
-		}
-		
-		public String getUsuario() {
-			return this.usuario;
-		}
-		public String getTipo() {
-			return this.tipo;
-		}
-		public long getPontos() {
-			return this.pontos;
-		}
-		
+	private String usuario;
+	private long pontos;
+	private String tipo;
+
+	Pontuacao(String usuario, long pontos, String tipo) throws PontuacaoInvalidaException {
+		validarPontuacao(usuario, pontos, tipo);
+		this.usuario = usuario;
+		this.pontos  = pontos;
+		this.tipo    = tipo;
 	}
+
+	Pontuacao(JSONObject jsonPontuacao) throws PontuacaoInvalidaException {
+		validarJSONObject(jsonPontuacao);
+		long pontos    = (long) jsonPontuacao.get("pontos");
+		String usuario = (String) jsonPontuacao.get("usuario");
+		String tipo    = (String) jsonPontuacao.get("tipo");
+		validarPontuacao(usuario, pontos, tipo);
+		this.usuario = usuario;
+		this.pontos  = pontos;
+		this.tipo    = tipo;
+	}
+
+	public void addPontos(long pontos) {
+		validarAdicionarPontos(pontos);
+		this.pontos += pontos;
+	}
+
+	public String toJSONString() {
+		return this.toJSONObject().toJSONString();
+	}
+
+	@SuppressWarnings("unchecked")
+	public JSONObject toJSONObject() {
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("usuario", usuario);
+		jsonObj.put("pontos", pontos);
+		jsonObj.put("tipo", tipo);
+		return jsonObj;
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public long getPontos() {
+		return this.pontos;
+	}
+
+	private void validarPontuacao(String usuario, long pontos, String tipo) {
+		validarPontos(pontos);
+		validarUsuario(usuario);
+		validarTipo(tipo);
+	}
+
+	private void validarPontos(long pontos) {
+		if (pontos < 1)
+			throw new PontuacaoInvalidaException("Pontos não podem ser inferiores a 1");
+	}
+
+	private void validarUsuario(String usuario) {
+		if (usuario.equals(""))
+			throw new PontuacaoInvalidaException("Usuário inválido (\"\")");
+		if (usuario.contains(" "))
+			throw new PontuacaoInvalidaException("Usuário inválido (possui espaço(s))");
+	}
+
+	private void validarTipo(String tipo) {
+		if (tipo.equals(""))
+			throw new PontuacaoInvalidaException("Tipo inválido (\"\")");
+		if (tipo.contains(" "))
+			throw new PontuacaoInvalidaException("Tipo inválido (possui espaço(s))");
+
+	}
+	
+	private void validarAdicionarPontos(long pontos)
+			throws PontuacaoInvalidaException {
+		if (pontos < 1)
+			throw new AdicionarPontosInvalidosException("Nao é possível adicionar pontos inferiores a 1");
+	}
+	
+	private void validarJSONObject(JSONObject jsonPontuacao) {
+		if (faltaChaveValida(jsonPontuacao))
+			throw new ObjetoJsonIvalidoException("Objeto Json em formato incorreto (faltando chave(s) valida(s))");
+		if (temValorDeTipoInvalido(jsonPontuacao))
+			throw new ObjetoJsonIvalidoException("Objeto Json em formato incorreto (valor(es) de tipo(s) invalido(s))");
+
+	}
+
+	private boolean faltaChaveValida(JSONObject jsonPontuacao) {
+		return (   jsonPontuacao.get("pontos")  == null 
+			    || jsonPontuacao.get("usuario") == null
+				|| jsonPontuacao.get("tipo")    == null);
+	}
+
+	private boolean temValorDeTipoInvalido(JSONObject jsonPontuacao) {
+		return !(  jsonPontuacao.get("usuario") instanceof String 
+				&& jsonPontuacao.get("pontos")  instanceof Long
+				&& jsonPontuacao.get("tipo")    instanceof String);
+	}
+
+}
